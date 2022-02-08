3f00c38baf9b8208def802402787ce861252bb7c
Ivaanfg <98523645+Ivaanfg@users.noreply.github.com>
2022-02-08 15:57:26 +0100

    Update Funcio_Oca.py

--- Funcio_Oca.py
+++ Funcio_Oca.py
@@ -1,39 +1,31 @@
-def tauler():
-    taulerr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-               11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-               21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
-               31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
-               41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
-               51, 52, 53, 54, 55, 56, 57, 58, 59, 60,
-               61, 62, 63]
-
-
 def dau():
     import random
-    llançar = print("Llançant el dau ...", "Ha sortit:", random.randint(1, 6))
-    return llançar
-#Exercici cultius per agafar de referencia per fer el taulell:
-def printdici(printdici):
-    for x in printdici:
-        for y in printdici[x]:
+    dado = random.randint(1, 6)
+    llancar = print("Llançant el dau ...", "Ha sortit:", dado )
+    return llancar
+
+
+def tauler():
+    for x in tauler:
+        for y in tauler[x]:
             print(y, end="")
         print()
     print()
+    return tauler
 
 
-def cult(joan, fila, columna, producto):
-    if joan[fila][columna] == "-":
-        joan[fila][columna] = "X"
-    else:
-        print("ja hi ha un cultiu alla. ")
-#from funcion import printdici,cult
-campo = {0:["","-","-","-","-"],
-        1:["-","-","-","-","-"],
-        2:["-","-","-","-","-"],
-        3:["-","-","-","-","-"],
-        4:["-","-","-","-","-"],
-        5:["-","-","-","-","-"]}
+def modtauler(taul, fila, columna, producto):
+    if taul[fila][columna] == "-":
+            taul[fila][columna] = "X"
 
-printdici(campo)
-cult(campo,2,3,"s")
-printdici(campo)
+    tablero = {0: ["", "-", "-", "-", "-"],
+               1: ["-", "-", "-", "-", "-"],
+               2: ["-", "-", "-", "-", "-"],
+               3: ["-", "-", "-", "-", "-"],
+               4: ["-", "-", "-", "-", "-"],
+               5: ["-", "-", "-", "-", "-"]}
+
+    tauler(tablero)
+    modtauler(tablero, 2, 3, "s")
+    tauler(tablero)
+    
