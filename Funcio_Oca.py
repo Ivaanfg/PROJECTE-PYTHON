f5f4c0254b81325b118097cb8298ccdc4ef015af
Ivaanfg <98523645+Ivaanfg@users.noreply.github.com>
2022-02-10 21:24:14 +0100

    Update Funcio_Oca.py

--- Funcio_Oca.py
+++ Funcio_Oca.py
@@ -9,48 +9,60 @@ def tauler():
 
     tablero = {0: ["[]", "[]", "[]", "[]", "2", "__", "[]", "[]", "2", "[]", "[]"],
                1: ["__", "[]", "2", "[]", "[]", "[]", "2", "--", "[]", "[]", "[]"],
-               2: ["2", "[]", "[]", "O", "2", "[]", "[]", "[]", "|_|", "2", "[]"],
+               2: ["2", "[]", "[]", "O", "2", "[]", "[]", "[]", "|__|", "2", "[]"],
                3: ["[]", "[]", "2", "[]", "[]", "[]", "[]", "2", "LA", "[]", "[]"],
                4: ["2", "[]", "[]", "[]", "[]", "2", "[]", "[]", "[]", "2", "[]"],
                5: ["||", "[]", "O", "2", "[]", "[]", "[]", ":(", "[]", "[]", "[]"]}
-
 
     for x in tablero:
         for y in tablero[x]:
             print(y, end="")
         print()
     print()
-
-
-print(tauler())
+    return tablero
 
 def fitxa_persona():
     #començar partida
-    start = input("Vols començar la partida ? S/N")
     fitxa1 = "0"
     fitxa2 = "1"
     da = dau()
-
     tau = tauler()
+    start = input("Vols començar la partida ? S/N")
+    
     while start != "N":
-        if start == "S":
-            print("Ara comença la partida...")
+        
+        print("Ara comença la partida...")
+        
         if da == 1:
-            tau.update(fitxa1[0]=[1])
+            tau.update(fitxa1[1])
         if da == 2:
-            tau.update(fitxa1[0]=[2])
+            tau.update(fitxa1[2])
         if da == 3:
-            tau.update(fitxa1[0]=[3])
+            tau.update(fitxa1[3])
         if da == 4:
-            tau.update(fitxa1[0]=[4])
+            tau.update(fitxa1[4])
         if da == 5:
-            tau.update(fitxa1[0]=[5])
+            tau.update(fitxa1[5])
         if da == 6:
-            tau.update(fitxa1[0]=[6])
+            tau.update(fitxa1[6])
     return tau
 print(fitxa_persona())
 
 
 
 
+def caselles_especials()
+    taul = tauler()
+    f1 = fi
+
+    ocas = taul(5,9,14,18,23,27,32,36,41,45,50,54,59)
+    puente = taul(6,12)
+    posada = taul(19)
+    pozo = taul(31)
+    laberinto = taul(42)
+    carcel = taul(56)
+    dados = taul(26,53)
+    calavera = taul(58)
+
+    forx
 
