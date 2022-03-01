d9856667a387b40f064ac84668fc000390be0d0a
Ivaanfg <98523645+Ivaanfg@users.noreply.github.com>
2022-03-01 16:36:12 +0100

    Update Funcio_Oca.py

--- Funcio_Oca.py
+++ Funcio_Oca.py
@@ -27,9 +27,10 @@ def fitxa(tau, posi, ID):
     anterior = 0
     if ID ==1:
         fitxa = "🟨"
+        print("Turn del JUGADOR:")
     else:
         fitxa ="🟥"
-    print("Turn del pc :")
+        print("Turn del pc :")
     da = dau()
     posi += da
     casella = tau[0][posi]
@@ -38,28 +39,28 @@ def fitxa(tau, posi, ID):
     llista_oca = [5,9,14,18,23,27,32,36,41,45,50,54,59]
     psoicions_buies  = [1,2,3,4,7,8,10,11,13,15,16,17,20,21,22,24,25,28,29,30,33,34,35,37,38,39,40,43,44,46,47,48,48,51,52,53,57,60,61,62,63,64]
     if posi_anterior == 0:
-       tau[0]="[START]"
+       tau[0][posi_anterior]="[START]"
     if posi_anterior in psoicions_buies:
-       tau[posi_anterior]="[]"
+       tau[0][posi_anterior]="[]"
     if posi_anterior in llista_oca:
-       tau[posi_anterior]="[🦆]"
-    if posi_anterior == 6 or 12:
-       tau[0]="[🌉]"
+       tau[0][posi_anterior]="[🦆]"
+    if posi_anterior == 6 or posi_anterior == 12:
+       tau[0][posi_anterior]="[🌉]"
     if posi_anterior == 19:
-       tau[0]="[🏠]"
+       tau[0][posi_anterior]="[🏠]"
     if posi_anterior == 31:
-       tau[0]="[🕳]"
+       tau[0][posi_anterior]="[🕳]"
     if posi_anterior == 42:
-       tau[0]="[🐍]"
+       tau[0][posi_anterior]="[🐍]"
     if posi_anterior == 56:
-       tau[0]="[⛓]"
-    if posi_anterior == 26 or 53:
-       tau[0]="[🎲]"
+       tau[0][posi_anterior]="[⛓]"
+    if posi_anterior == 26 or posi_anterior == 53:
+       tau[0][posi_anterior]="[🎲]"
     if posi_anterior == 58:
-       tau[0]="[💀]"    
+       tau[0][posi_anterior]="[💀]"
     if posi_anterior == 65:
-       tau[0]="[END]"
-       
+       tau[0][posi_anterior]="[END]"
+
     return tau, posi,ID
 
 def partida():
@@ -71,30 +72,13 @@ def partida():
         f2 = tauler()
         posi1 = 0
         posi2 = 0
-        while posi1 <= 63:
+        while posi1 <= 65:
             pregunta = input("tirar S/N")
             if (pregunta == "S"):
                 f1, posi1,ID = fitxa(f2, posi1,1)
                 print(f1)
                 f2, posi2,ID = fitxa(f1, posi2,2)
                 print(f2)
+    return f2
 
-
-"""
-def caselles_especials()
-    taul = tauler()
-    f1 = fi
-
-    ocas = taul(5,9,14,18,23,27,32,36,41,45,50,54,59)
-    puente = taul(6,12)
-    posada = taul(19)
-    pozo = taul(31)
-    laberinto = taul(42)
-    carcel = taul(56)
-    dados = taul(26,53)
-    calavera = taul(58)
-
-    forx
-
-
-"""
+""""def caselles_especials():"""
