7232e980491672a31125345c3dcaac66f525838e
Ivaanfg <98523645+Ivaanfg@users.noreply.github.com>
2022-03-24 21:15:09 +0100

    Update Quien_es_quien.py

--- Quien_es_quien.py
+++ Quien_es_quien.py
@@ -1,22 +1,22 @@
 import random
 
-tableropc = {'Ivan': ["home", "pel curt", "pel negre", "ulls blaus", "nas petit","té barba"],
-             'Joan': ["home", "pel curt", "pel roig", "ulls negres", "nas gran","té barba"],
-             'Arnau': ["home", "pel curt", "pel negre", "ulls negres", "porta ulleres", "nas gran","té barba"],
-             'Teresa': ["dona", "pel llarg", "pel roig", "ulls negres", "nas petit","té anelleta"],
-             'Maria': ["dona", "pel llarg", "pel roig", "ulls blaus", "porta ulleres", "nas gran","té analleta"],
-             'Susana': ["dona", "pel llarg", "pel negre", "ulls negres", "nas petit"],
-             'Ramon': ["home", "pel llarg", "pel roig", "ulls blaus", "porta ulleres", "nas gran"],
-             'Elisabeth': ["dona", "pel curt", "pel negre", "ulls negres", "porta ulleres", "nas petit"]}
+tableropc = {'Ivan': ["home", "pel curt", "pel negre", "ulls blaus","no porta ulleres","nas petit","té barba","no té anelleta"],
+             'Joan': ["home", "pel curt", "pel roig", "ulls negres","no porta ulleres","nas gran", "té barba","no té anelleta"],
+             'Arnau': ["home", "pel curt", "pel negre", "ulls negres", "porta ulleres", "nas gran", "té barba","no té anelleta"],
+             'Teresa': ["dona", "pel llarg", "pel roig", "ulls negres","no porta ulleres", "nas petit","no té barba","té anelleta"],
+             'Maria': ["dona", "pel llarg", "pel roig", "ulls blaus", "porta ulleres", "nas gran","no té barba","té analleta"],
+             'Susana': ["dona", "pel llarg", "pel negre", "ulls negres","no porta ulleres", "nas petit","no té barba","no té anelleta"],
+             'Ramon': ["home", "pel llarg", "pel roig", "ulls blaus", "porta ulleres", "nas gran","no té barba","no té anelleta"],
+             'Elisabeth': ["dona", "pel curt", "pel negre", "ulls negres", "porta ulleres", "nas petit","no té barba","no té anelleta"]}
 
-tablerouser = {'Ferran': ["home", "pel curt", "pel roig", "ulls blaus", "nas petit","té barba"],
-               'Miquel': ["home", "pel curt", "pel roig", "ulls negres", "nas gran"],
-               'Llorenç': ["home", "pel curt", "pel negre", "ulls negres", "porta ulleres", "nas gran","té barba"],
-               'Ramona': ["dona", "pel llarg", "pel roig", "ulls negres", "nas petit"],
+tablerouser = {'Ferran': ["home", "pel curt", "pel roig", "ulls blaus", "nas petit", "té barba","no té anelleta"],
+               'Miquel': ["home", "pel curt", "pel roig", "ulls negres", "nas gran","no té barba","no té anelleta"],
+               'Llorenç': ["home", "pel curt", "pel negre", "ulls negres", "porta ulleres", "nas gran", "té barba","no té anelleta"],
+               'Ramona': ["dona", "pel llarg", "pel roig", "ulls negres", "nas petit","no té barba","no té anelleta"],
                'Alicia': ["dona", "pel llarg", "pel negre", "ulls negres", "porta ulleres", "nas gran","porta anelleta"],
-               'Abril': ["dona", "pel llarg", "pel negre", "ulls blaus", "nas petit"],
-               'Albert': ["home", "pel llarg", "pel roig", "ulls blaus", "porta ulleres", "nas gran","té barba"],
-               'Toni': ["dona", "pel curt", "pel negre", "ulls negres", "porta ulleres", "nas petit","porta anelleta"]}
+               'Abril': ["dona", "pel llarg", "pel negre", "ulls blaus", "nas petit","no té barba","no té anelleta"],
+               'Albert': ["home", "pel llarg", "pel roig", "ulls blaus", "porta ulleres", "nas gran", "té barba","no té anelleta"],
+               'Toni': ["dona", "pel curt", "pel negre", "ulls negres", "porta ulleres", "nas petit","no té barba","porta anelleta"]}
 
 preguntes = ["1 : ÉS HOME?","2 : PEL LLARG?", "3 : PEL NEGRE?","5 : ULLS NEGRES?",
              "6 : PORTA ULLERES?", "7 : NAS GRAN?","TÉ BARBA?","9 : PORTA ANELLETA?"]
@@ -48,29 +48,5 @@ def buclePc():
         print(pregunta_pc)
         resposta = input()
         pregunta(pregunta_num, resposta)
-
-        if resposta == "si" in preguntes[0]: 
-            for x in tablerouser:
-                for y in tablerouser[x]:
-                    if tablerouser[y] == "dona":
-                        tablerouser.pop(x)
-
-        if resposta == "si" in preguntes[1]:
-            
-        if resposta == "si" in preguntes[2]:
-            
-        if resposta == "si" in preguntes[3]:
-            
-        if resposta == "si" in preguntes[4]:
-            
-        if resposta == "si" in preguntes[5]:
-            
-        if resposta == "si" in preguntes[6]:
-            
-        if resposta == "si" in preguntes[7]:
-            
-        print(tablerouser)
-
-
 
 buclePc()
